eef953b4a9ef91c6f3eb38a9d0ef1c840359eb42
Soylemez <wb_omer@hotmail.com>
2023-01-07 23:28:53 +0300

    areachart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { fetchCountries } from './api';
+import AreaChart from './components/AreaChart';
+
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 import covidLogo from "./covid.png";
 
@@ -20,10 +25,33 @@ const App = () => {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Grid container>
+        <Grid container sx= {{ mb: 4}}>
           <img src={covidLogo} alt="Covid 19" style={{
             marginTop:15
           }} />
+        
+
+        <FormControl sx={{ mt: 4, ml:5, minWidth: 300 }} size="medium">
+          <Select
+            labelId="countries"
+            id="countries"
+            value={country}
+            label="country"
+            onChange={(event) => setCountry(event.target.value)}
+          >
+            {
+              countries.map(country => (
+                <MenuItem value={country.Slug}>{country.Country}</MenuItem>
+              ))
+            }
+            
+          </Select>
+        
+      </FormControl>
+      </Grid>
+        
+        <Grid item xs={12}>
+              <AreaChart />
         </Grid>
       </Container>
     </React.Fragment>
--- src/components/AreaChart.jsx
+++ src/components/AreaChart.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Chart from 'react-apexcharts';
 
 const AreaChart = () => {
     return (
-        <div>
-
-        </div>
+        this.state = {
+          
+            series: [{
+              name: 'series1',
+              data: [31, 40, 28, 51, 42, 109, 100]
+            }, {
+              name: 'series2',
+              data: [11, 32, 45, 32, 34, 52, 41]
+            }],
+            options: {
+              chart: {
+                height: 350,
+                type: 'area'
+              },
+              dataLabels: {
+                enabled: false
+              },
+              stroke: {
+                curve: 'smooth'
+              },
+              xaxis: {
+                type: 'datetime',
+                categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+              },
+              tooltip: {
+                x: {
+                  format: 'dd/MM/yy HH:mm'
+                },
+              },
+            },
+        }
     )
 }
 
